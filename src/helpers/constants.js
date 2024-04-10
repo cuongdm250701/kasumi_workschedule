8577849d26bb9cf7f06cd7780ebe02a56df4cfb6
cuongdm250701 <domanhcuong250701@gmail.com>
2024-04-10 10:57:30 -0700

    setting init data default public holiday

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -13,15 +13,186 @@ const REST_FULL_API_CODE = {
     message: "Invalid token or expires",
   },
 };
-const LIST_PUBLIC_HOLIDAY_DEFAULT = [
+const LIST_PUBLIC_HOLIDAY_SPECIFIC = [
   {
     type: 1,
     month: 1,
     date: 1,
-    year: new Date().getFullYear(),
-    event_detail: "元旦",
+    event_details: "元旦",
+  },
+  {
+    type: 1,
+    month: 2,
+    date: 11,
+    event_details: "建国記念日",
+  },
+  {
+    type: 1,
+    month: 3,
+    date: 20,
+    event_details: "春分の日",
+  },
+  {
+    type: 1,
+    month: 4,
+    date: 29,
+    event_details: "昭和の日",
+  },
+  {
+    type: 1,
+    month: 5,
+    date: 3,
+    event_details: "憲法記念日",
+  },
+  {
+    type: 1,
+    month: 5,
+    date: 4,
+    event_details: "みどりの日",
+  },
+  {
+    type: 1,
+    month: 5,
+    date: 5,
+    event_details: "こどもの日",
+  },
+  {
+    type: 1,
+    month: 8,
+    date: 11,
+    event_details: "山の日",
+  },
+  {
+    type: 1,
+    month: 9,
+    date: 22,
+    event_details: "国民の休日",
+  },
+  {
+    type: 1,
+    month: 11,
+    date: 3,
+    event_details: "文化の日",
+  },
+  {
+    type: 1,
+    month: 11,
+    date: 23,
+    event_details: "勤労感謝の日",
   },
 ];
+
+const LIST_PUBLIC_HOLIDAY_NOT_SPECIFIC = [
+  {
+    day: 1,
+    week: 2,
+    month: 0,
+    type: 1,
+    event_details: "成人の日",
+  },
+  {
+    day: 0,
+    week: 2,
+    month: 4,
+    type: 2,
+    event_details: "母の日",
+  },
+  {
+    day: 0,
+    week: 3,
+    month: 5,
+    type: 2,
+    event_details: "父の日",
+  },
+  {
+    day: 1,
+    week: 3,
+    month: 6,
+    type: 1,
+    event_detail: "海の日",
+  },
+  {
+    day: 1,
+    week: 3,
+    month: 8,
+    type: 1,
+    event_details: "敬老の日",
+  },
+  {
+    day: 1,
+    week: 2,
+    month: 9,
+    type: 1,
+    event_details: "体育の日",
+  },
+];
+
+const LIST_EVENT_SPECIFIC = [
+  {
+    type: 2,
+    month: 2,
+    date: 3,
+    event_details: "節分",
+  },
+  {
+    type: 2,
+    month: 2,
+    date: 14,
+    event_details: "ﾊﾞﾚﾝﾀｲﾝﾃﾞｰ",
+  },
+  {
+    type: 2,
+    month: 3,
+    date: 3,
+    event_details: "ひなまつり",
+  },
+  {
+    type: 2,
+    month: 3,
+    date: 14,
+    event_details: "ホワイトデー",
+  },
+  {
+    type: 2,
+    month: 7,
+    date: 7,
+    event_details: "七夕",
+  },
+  {
+    type: 2,
+    month: 8,
+    date: 13,
+    event_details: "お盆入り",
+  },
+  {
+    type: 2,
+    month: 8,
+    date: 15,
+    event_details: "お盆",
+  },
+  {
+    type: 2,
+    month: 12,
+    date: 24,
+    event_details: "ｸﾘｽﾏｽ･ｲﾌﾞ",
+  },
+  {
+    type: 2,
+    month: 12,
+    date: 25,
+    event_details: "クリスマス",
+  },
+  {
+    type: 2,
+    month: 12,
+    date: 31,
+    event_details: "大晦日",
+  },
+];
+
 module.exports = {
   REST_FULL_API_CODE,
+  LIST_PUBLIC_HOLIDAY_NOT_SPECIFIC,
+  LIST_PUBLIC_HOLIDAY_SPECIFIC,
+  LIST_EVENT_SPECIFIC,
 };
--- src/helpers/get-public-holiday.js
+++ src/helpers/get-public-holiday.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+const { LIST_PUBLIC_HOLIDAY_SPECIFIC } = require("./constants");
+
 const getListPublicHoliday = async (countryCode, year) => {
   try {
     const publicHoliday = await axios.get(
@@ -8,6 +10,78 @@ const getListPublicHoliday = async (countryCode, year) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+function getSpecificWeekdayOfMonth(year, month, targetDay, targetWeek) {
+  const firstDayOfMonth = new Date(year, month, 1);
+  const firstWeekday = firstDayOfMonth.getDay();
+  console.log(firstWeekday);
+  let offset = targetDay - firstWeekday;
+  console.log(offset);
+  if (offset < 0) {
+    offset += 7;
+  }
+  const targetDate = 1 + offset + (targetWeek - 1) * 7;
+  console.log(targetDate);
+  return new Date(year, month, targetDate);
+}
+// Lấy ngày thứ 2 tuần thứ 2 của tháng 1 năm 2024
+const year = 2024;
+// Tháng 1 (JavaScript đếm tháng từ 0)
+const month = 0;
+// Thứ 2 (0 là Chủ Nhật, 1 là Thứ 2, ..., 6 là Thứ 7)
+const targetDay = 1;
+// Tuần thứ 2
+const targetWeek = 2;
+const secondMondayOfJanuary2024 = getSpecificWeekdayOfMonth(
+  year,
+  month,
+  targetDay,
+  targetWeek
+);
+console.log(secondMondayOfJanuary2024.toDateString());
+
+// const extraDaysOff = () => {
+//   const date = new Date();
+//   const listPublicHoliday = LIST_PUBLIC_HOLIDAY_SPECIFIC;
+//   const dayOff = [];
+//   const currentYear = date.getFullYear();
+//   listPublicHoliday.forEach((item) => {
+//     const isSunday = new Date(currentYear, item.month - 1, item.date).getDay()
+//       ? false
+//       : true;
+//     if (isSunday) {
+//       const addDayOff = {
+//         type: 1,
+//         month: item.month,
+//         date: item.date + 1,
+//         event_details: "振替休日",
+//       };
+//       dayOff.push(addDayOff);
+//     }
+//   });
+//   return [...listPublicHoliday, ...dayOff];
+// };
+const extraDaysOff = () => {
+  const date = new Date();
+  const listPublicHoliday = [...LIST_PUBLIC_HOLIDAY_SPECIFIC];
+  const currentYear = date.getFullYear();
+  listPublicHoliday.forEach((item) => {
+    const isSunday = new Date(currentYear, item.month - 1, item.date).getDay()
+      ? false
+      : true;
+    if (isSunday) {
+      const dayOff = {
+        type: 1,
+        month: item.month,
+        date: item.date + 1,
+        event_details: "振替休日",
+      };
+      listPublicHoliday.push(dayOff);
+    }
+  });
+  console.log(listPublicHoliday);
+  return listPublicHoliday;
 };
 
 module.exports = { getListPublicHoliday };
